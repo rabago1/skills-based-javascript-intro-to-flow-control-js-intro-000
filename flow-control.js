7530416b21c6b8b7c22c519909af314455ca8c56
Pat Rabago <patriciorabago@gmail.com>
2019-02-04 22:44:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,11 @@
 function basicTeenager(age) {
-  if (age >=13 && age <=19) {
-  return "You are a teenager!"
-}
+  if (age >=13 && age <=19) {return "You are a teenager!"}
 }
 
 
-function teenager(age) {
-
+function teenager(age) { 
+  if(age >=13 && age <=19) {return "You are a teenager!"}
+  else {return "You are not a teenager"}
 }
 
 function ageChecker(age) {
