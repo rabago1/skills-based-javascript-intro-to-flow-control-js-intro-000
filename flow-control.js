860babd6eb945da5603d3936680bdb132f50fd7f
Pat Rabago <patriciorabago@gmail.com>
2019-02-04 23:23:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,15 +25,15 @@ function switchAge(age) {
     return "You are a teenager"
     case 14:
     return "You are a teenager"
-    case 15
+    case 15:
     return "You are a teenager"
-    case 16
+    case 16:
     return "You are a teenager"
-    case 17
+    case 17:
     return "You are a teenager"
-    case 18
+    case 18:
     return "You are a teenager"
-    case 19
+    case 19:
     return "You are a teenager"
     default:
     return "You have an age"
